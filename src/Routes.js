390ee455e7553d008e51bdc6c1b30c859e6aa3de
draganamitrovic <dragana.mitrovic.94@gmail.com>
2017-11-24 15:37:40 +0100

    dodat temp1

--- src/Routes.js
+++ src/Routes.js
@@ -5,9 +5,10 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from '../Home';
 import Settings from '../Settings';
-import FullText from '../temp4_fulltext';
+import ImageButtons from '../temp1_imagebuttons';
 import FullImage from '../temp2_fullimage';
 import TextImage from '../temp3_textimage';
+import FullText from '../temp4_fulltext';
 
 export default class Routes extends React.Component {
   render() {
@@ -18,9 +19,10 @@ export default class Routes extends React.Component {
           <Scene key="signup" component={Signup} title="Register" />
           <Scene key="home" component={Home} title="Home" />
           <Scene key="settings" component={Settings} title="Settings" />
-          <Scene key="temp4" component={FullText} title="Text temlpate" />
+          <Scene key="temp1" component={ImageButtons} title="Image and buttons temlpate" />
           <Scene key="temp2" component={FullImage} title="Image template" />
           <Scene key="temp3" component={TextImage} title="Text and image Template" />
+          <Scene key="temp4" component={FullText} title="Text temlpate" />
         </Stack>
       </Router>
     )
